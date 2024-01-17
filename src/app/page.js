5df7c5d14c48e97df283424af36c085d83f39ea1
Luke Fox <lukefox5@gmail.com>
2024-01-17 19:16:52 +0000

    added mui and with emotion

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,7 @@
 'use client'
 
-import Image from 'next/image'
 import useSWR from 'swr'
-import styles from './page.module.css'
+import { Typography, Grid } from '@mui/material'
 
 const fetcher = (url) => fetch(url).then((r) => r.json())
 
@@ -11,43 +10,10 @@ export default function Home() {
   const { data, error, isLoading } = useSWR('/api/folders', fetcher)
 
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By
-            {' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
+    <main>
+      <Grid container alignItems="center" justifyContent="center" sx={{ height: '100vh' }}>
+        <Typography variant="h5">Bright HR Tech Test</Typography>
+      </Grid>
     </main>
   )
 }
