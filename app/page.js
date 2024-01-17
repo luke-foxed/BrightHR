024849cb8a8a175abdbf9b46ca04f604c916bc6d
Luke Fox <lukefox5@gmail.com>
2024-01-17 21:48:40 +0000

    improved grid for showing items

--- app/page.js
+++ app/page.js
@@ -1,46 +1,81 @@
 'use client'
 
 import useSWR from 'swr'
-import { Typography, Grid } from '@mui/material'
+import { Typography, Grid, styled, Box, Autocomplete, TextField } from '@mui/material'
+import { useState } from 'react'
 import ItemIcon from './components/item_icon'
+
+const StyledContainerBox = styled(Box)({
+  display: 'grid',
+  alignContent: 'center',
+  justifyContent: 'center',
+  gridTemplateRows: '0.4fr 1fr',
+  height: '100vh',
+  gap: '10%',
+  textAlign: 'center',
+})
+
+const StyledItemBox = styled(Box)({
+  width: '100vw',
+  padding: '50px',
+  minHeight: '100%',
+  backgroundColor: 'blue',
+  borderTopLeftRadius: '40px',
+  borderTopRightRadius: '40px',
+})
+
+const StyledItem = styled(Grid)({
+  borderRadius: '10px',
+  padding: '10px',
+  height: '140px',
+  backgroundColor: '#fff',
+})
 
 const fetcher = (url) => fetch(url).then((r) => r.json())
 
 export default function Home() {
   // eslint-disable-next-line no-unused-vars
   const { data, error, isLoading } = useSWR('/api/folders', fetcher)
+  const [searchString, setSearchString] = useState('')
+  const [sorting, setSorting] = useState({ field: '', order: 'asc' })
+  const [sortedItems, setSortedItems] = useState([])
 
   return (
     <main>
-      <Grid container alignItems="center" justifyContent="center" sx={{ height: '100vh' }}>
-        <Typography variant="h5">Bright HR Tech Test</Typography>
+      <StyledContainerBox>
+        <Grid container item alignItems="center" justifyContent="center">
+          <Typography variant="h5">Bright HR Tech Test</Typography>
+        </Grid>
+
         {isLoading ? (
           'Loading'
         ) : (
-          <Grid container spacing={2} alignItems="center" justifyContent="flex-start">
-            {data.folders.map((folder) => (
-              <Grid
-                key={`${folder.name}_${folder.added}`}
-                xs={4}
-                md={2}
-                sx={{ padding: '10px' }}
-              >
-                <Grid
-                  container
-                  direction="column"
-                  item
-                  style={{ padding: '10px', background: '#fff' }}
-                  alignItems="center"
-                  justifyContent="center"
-                >
-                  <ItemIcon type={folder.type} />
-                  <Typography>{folder.name}</Typography>
+          <StyledItemBox>
+            <Autocomplete
+              sx={{ width: '300px' }}
+              ptions={data.folders.map((item) => item.name)}
+              disablePortal
+              renderInput={(params) => <TextField {...params} label="Search Items..." />}
+            />
+            <Grid container alignItems="center" justifyContent="flex-start">
+              {data.folders.map((item) => (
+                <Grid key={`${item.name}_${item.added}`} xs={3} md={2} sx={{ padding: '10px' }}>
+                  <StyledItem
+                    container
+                    direction="column"
+                    item
+                    alignItems="center"
+                    justifyContent="center"
+                  >
+                    <ItemIcon type={item.type} />
+                    <Typography>{item.name}</Typography>
+                  </StyledItem>
                 </Grid>
-              </Grid>
-            ))}
-          </Grid>
+              ))}
+            </Grid>
+          </StyledItemBox>
         )}
-      </Grid>
+      </StyledContainerBox>
     </main>
   )
 }
